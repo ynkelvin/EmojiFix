f9ae0de5fabcafd32d04eab274fd43f7ac8ebed2
unknown <kelvinyeungnk@gmail.com>
2015-05-02 20:57:13 +0800

    remove extra code:

--- replace.js
+++ replace.js
@@ -1,15 +1,12 @@
-console.log(localStorage['enabled']);
-if (localStorage['enabled'] != "false") {
-	var css = ".emoji {  background-image: url('"+chrome.extension.getURL('images/d80a3fdced298fcbff1f850e74b3e687.png')+"');}.k .emoji {  background-image: url('"+chrome.extension.getURL('images/1a4a846f5fc7397877db9231c84b94d7.jpg')+"');}",
-	    head = document.head || document.getElementsByTagName('head')[0],
-	    style = document.createElement('style');
+var css = ".emoji {  background-image: url('"+chrome.extension.getURL('images/d80a3fdced298fcbff1f850e74b3e687.png')+"');}.k .emoji {  background-image: url('"+chrome.extension.getURL('images/1a4a846f5fc7397877db9231c84b94d7.jpg')+"');}",
+    head = document.head || document.getElementsByTagName('head')[0],
+    style = document.createElement('style');
 
-	style.type = 'text/css';
-	if (style.styleSheet){
-	  style.styleSheet.cssText = css;
-	} else {
-	  style.appendChild(document.createTextNode(css));
-	}
-
-	head.appendChild(style);
+style.type = 'text/css';
+if (style.styleSheet){
+  style.styleSheet.cssText = css;
+} else {
+  style.appendChild(document.createTextNode(css));
 }
+
+head.appendChild(style);
