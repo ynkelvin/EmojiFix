e11b999e430d93b68259c114680a4779a9136e54
unknown <kelvinyeungnk@gmail.com>
2015-05-09 22:00:30 +0800

    WhatsApp updated emoji

--- Chrome/replace.js
+++ Chrome/replace.js
@@ -1,4 +1,4 @@
-var css = ".emoji {  background-image: url('"+chrome.extension.getURL('images/d80a3fdced298fcbff1f850e74b3e687.png')+"');}.k .emoji {  background-image: url('"+chrome.extension.getURL('images/1a4a846f5fc7397877db9231c84b94d7.jpg')+"');}",
+var css = ".emoji-small-loaded .emoji {  background-image: url('"+chrome.extension.getURL('images/d80a3fdced298fcbff1f850e74b3e687.png')+"');}.emoji-large-loaded .k .emoji {  background-image: url('"+chrome.extension.getURL('images/1a4a846f5fc7397877db9231c84b94d7.jpg')+"');}",
     head = document.head || document.getElementsByTagName('head')[0],
     style = document.createElement('style');
 
